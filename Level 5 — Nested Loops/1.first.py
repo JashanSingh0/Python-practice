e1fd584256250f7f2ba0fcd91c54d5a8b8a5ac0e
JashanSingh0 <singhsherry2003@.com>
2026-09-08 12:51:49 +0530

    Add Level 5 - Nested Loops

---
+++ Level 5 — Nested Loops/1.first.py
@@ -0,0 +1,28 @@
+# Print a square of * of size n.
+n= int(input("Enter side: "))
+for i in range(n):
+    print(n*'* ')
+
+
+# Print a rectangle of * with given rows and columns.
+l= int(input("Enter length: "))
+b= int(input("Enter width: "))
+for i in range(b):
+    print(l*'* ')
+
+# Print a right triangle of *.
+n= int(input("Enter side: "))
+for i in range(1,n+1):
+    print(i*'* ')
+
+# Print an inverted right triangle.
+n= int(input("Enter side: "))
+for i in range(n,0,-1):
+    print(i*'* ')
+
+# Print a triangle containing numbers.
+n= int(input("Enter side: "))
+for i in range(1,n+1):
+    for j in range(i):
+        print(i, end=' ')
+    print()
