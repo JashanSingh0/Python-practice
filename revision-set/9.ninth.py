3abacc2e61ae87019b6f4ca97c0a4eadfb0f513d
JashanSingh0 <singhsherry2003@.com>
2026-09-11 17:55:34 +0530

    Add new practice questions

---
+++ revision-set/9.ninth.py
@@ -0,0 +1,13 @@
+# Q230. Check whether a list is sorted recursively.
+
+
+# Q134. Function to calculate GCD.
+
+
+# Q158. Function that searches for an element in a list.
+
+
+# Q212. Print a string in reverse recursively.
+
+
+# Q121. Function to calculate factorial.
